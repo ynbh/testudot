b94100a5632586e2c95d935012e8bde48575e762
rwlc <yshsbhat@outlook.com>
2024-12-02 15:05:05 -0500

    fix: fetch req and unnecessary imports

--- monitor.ts
+++ monitor.ts
@@ -1,9 +1,7 @@
-import puppeteer, { Browser } from "puppeteer";
 import { parse } from "node-html-parser";
 import fs from "fs";
 import nodemailer from "nodemailer";
 import cron from "node-cron";
-import { createClient } from "@supabase/supabase-js";
 import sqlite3 from "sqlite3";
 import { open, Database } from "sqlite";
 
@@ -73,24 +71,8 @@ const initializeDatabase = async () => {
 const getTestudoCourseHTML = async(courseName: string) => {
   const req = await fetch(`https://app.testudo.umd.edu/soc/search?courseId=${courseName}&sectionId=&termId=202501&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on`, {
     "headers": {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-      "cache-control": "max-age=0",
-      "priority": "u=0, i",
-      "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"macOS\"",
-      "sec-fetch-dest": "document",
-      "sec-fetch-mode": "navigate",
-      "sec-fetch-site": "same-origin",
-      "sec-fetch-user": "?1",
-      "upgrade-insecure-requests": "1",
-      "cookie": "JSESSIONID=E1BF62D35A5C0BA8583315BA0799713D; _ga_26KY7YZR70=GS1.1.1699823535.2.0.1699823535.0.0.0; _ga=GA1.3.239578705.1699654926; nmstat=84e4bba9-0213-fc21-27a4-7e25f5ed8bb6; _ga_9W00B8Y6D6=GS1.1.1702360504.2.0.1702360504.0.0.0; _ga_FWRTZ8V96T=GS1.1.1702416504.2.0.1702416504.60.0.0; _hjSessionUser_2558111=eyJpZCI6IjE5MzhkMWU1LTQwYTQtNWY4My04NTQ4LWVjYWZlNmFmMzJmZCIsImNyZWF0ZWQiOjE3MDIzNDQ2MzEyMjYsImV4aXN0aW5nIjp0cnVlfQ==; _ga_0ME57WCM7H=GS1.1.1702514387.3.0.1702514395.52.0.0; _ga_6RJ22ZEGTP=GS1.1.1702516135.1.1.1702516144.0.0.0; _ga_0HWJZKFYZR=GS1.1.1702579495.8.0.1702579497.0.0.0; _ga=GA1.2.239578705.1699654926; _ga_E3TLPTPH8G=GS1.2.1702598269.3.1.1702598272.0.0.0; _ga_E9M33B2469=GS1.1.1702608386.4.0.1702608386.0.0.0; visid_incap_2811896=0MSvot8VTdWilqwsBOmmV8aSvmUAAAAAQUIPAAAAAAD6MEodHVc7Sd91v8LOdsRn; dtCookie=v_4_srv_16_sn_A1900212548BC69878EE4B229CB38983_perc_100000_ol_0_mul_1_app-3Af9eed1d550ab4737_1_app-3A5718141a4da56b27_1; my-saved-list=%7B%22202501-CMSC351-0301%22%3A%7B%22termId%22%3A%22202501%22%2C%22courseId%22%3A%22CMSC351%22%2C%22sectionId%22%3A%220301%22%7D%2C%22202501-STAT400-0122%22%3A%7B%22termId%22%3A%22202501%22%2C%22courseId%22%3A%22STAT400%22%2C%22sectionId%22%3A%220122%22%7D%2C%22202501-INST104-0101%22%3A%7B%22termId%22%3A%22202501%22%2C%22courseId%22%3A%22INST104%22%2C%22sectionId%22%3A%220101%22%7D%2C%22202501-CMSC320-0101%22%3A%7B%22termId%22%3A%22202501%22%2C%22courseId%22%3A%22CMSC320%22%2C%22sectionId%22%3A%220101%22%7D%2C%22202501-CMSC398L-0101%22%3A%7B%22termId%22%3A%22202501%22%2C%22courseId%22%3A%22CMSC398L%22%2C%22sectionId%22%3A%220101%22%7D%2C%22202501-CMSC330-0103%22%3A%7B%22termId%22%3A%22202501%22%2C%22courseId%22%3A%22CMSC330%22%2C%22sectionId%22%3A%220103%22%7D%7D; monkey=6757c2c3-9fb2-4dcd-9d35-4acc714b30f1; JSESSIONID=CB0F63B467E7E39A70E881712726221B; AWSALB=bMtonbBrev+b9V2SMugUKNjE9jj9P4jxQdDuMVIlRpO1nGhvTZuGtW/m/tcgjCc386QbM/A7rfegvzIChW/0VrJMcH6y4Siibfu3WXOCBel31Wzd6jSEyoJq/+eO; AWSALBCORS=bMtonbBrev+b9V2SMugUKNjE9jj9P4jxQdDuMVIlRpO1nGhvTZuGtW/m/tcgjCc386QbM/A7rfegvzIChW/0VrJMcH6y4Siibfu3WXOCBel31Wzd6jSEyoJq/+eO",
-      "Referer": "https://app.testudo.umd.edu/soc/",
-      "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    "body": null,
-    "method": "GET"
+      "User-Agent": "testudot/0.0.0"
+    }
   });
   const data = await req.text();
   return data;
@@ -384,8 +366,8 @@ async function sendNotification(changes, courseName: string) {
 
   // send the email with HTML content
   await transporter.sendMail({
-    from: process.env.EMAIL_PASS,
-    to: "someone@gmail.com",
+    from: process.env.EMAIL_USER,
+    to: process.env.TO_USER,
     subject: `🔔 Changes Detected in ${courseName} Sections!`,
     html: emailBody,
   });
